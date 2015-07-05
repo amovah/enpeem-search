f6711094eb4d0a2bbc57afbf55a6d6fa2cdf49e9
Ali Movahedi <aligeekjs@gmail.com>
2015-07-05 18:48:41 +0430

    Initial  commit

---
+++ search-npm.js
@@ -0,0 +1,35 @@
+import request from 'request';
+import cheerio from 'cheerio';
+
+export default function(pack, limit = 1, offset = 0) {
+  const URL = `https://www.npmjs.com/`
+  const METHOD = `search?q=${pack}`;
+  return new Promise((resolve, reject) => {
+    request(URL + METHOD, (err, res, body) => {
+      const $ = cheerio.load(body);
+      const Data = (index) => {
+        const BASE = `.search-results > li:nth-child(${index}) >`;
+        let result = {};
+
+        result.name =  $(`${BASE} div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)`).attr('href').split('/')[2];
+        result.author =  $(`${BASE} div:nth-child(1) > h3:nth-child(1) > a:nth-child(2)`).attr('href').split('/~')[1];
+        result.description =  $(`${BASE} div:nth-child(1) > p:nth-child(2)`).text();
+        result.stars =  $(`${BASE} div:nth-child(1) > p:nth-child(3) > span:nth-child(1)`).text();
+        result.version = $(`${BASE} div:nth-child(1) > p:nth-child(3) > span:nth-child(2)`).text().slice(1);
+        result.html_url = `${URL}package/${result.name}`;
+
+        return result;
+      }
+      try {
+        let result = [];
+        while (offset < 20 && result.length < limit) {
+          result.push(new Data(offset + 1));
+          ++offset;
+        }
+        resolve(result);
+      } catch(e) {
+        reject(new Error('Package not found'));
+      }
+    });
+  });
+}
