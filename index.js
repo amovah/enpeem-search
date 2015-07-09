b53c20ad0f9d95ff6f31ba7895c9c60b636b86b3
amovah <aligeekjs@gmail.com>
2015-07-09 15:17:36 +0430

    Build unbuild.js file

--- index.js
+++ index.js
@@ -26,28 +26,24 @@ exports['default'] = function (pack) {
       var Data = function Data(index) {
         try {
           var BASE = '.search-results > li:nth-child(' + index + ') >';
-          var result = {};
-          result.name = $(BASE + ' div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').attr('href').split('/')[2];
-          result.author = $(BASE + ' div:nth-child(1) > h3:nth-child(1) > a:nth-child(2)').attr('href').split('/~')[1];
-          result.description = $(BASE + ' div:nth-child(1) > p:nth-child(2)').text();
-          result.stars = $(BASE + ' div:nth-child(1) > p:nth-child(3) > span:nth-child(1)').text();
-          result.version = $(BASE + ' div:nth-child(1) > p:nth-child(3) > span:nth-child(2)').text().slice(1);
-          result.url = URL + 'package/' + result.name;
-          return result;
+          var _result = {};
+          _result.name = $(BASE + ' div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').attr('href').split('/')[2];
+          _result.author = $(BASE + ' div:nth-child(1) > h3:nth-child(1) > a:nth-child(2)').attr('href').split('/~')[1];
+          _result.description = $(BASE + ' div:nth-child(1) > p:nth-child(2)').text();
+          _result.stars = $(BASE + ' div:nth-child(1) > p:nth-child(3) > span:nth-child(1)').text();
+          _result.version = $(BASE + ' div:nth-child(1) > p:nth-child(3) > span:nth-child(2)').text().slice(1);
+          _result.url = URL + 'package/' + _result.name;
+          return _result;
         } catch (e) {
           return;
         }
       };
-      try {
-        var result = [];
-        while (offset < 20 && result.length < limit) {
-          if (Data(offset + 1)) result.push(Data(offset + 1));
-          ++offset;
-        }
-        resolve(result);
-      } catch (e) {
-        reject([]);
+      var result = [];
+      while (offset < 20 && result.length < limit) {
+        if (Data(offset + 1)) result.push(Data(offset + 1));
+        ++offset;
       }
+      result.length ? resolve(result) : reject();
     });
   });
 };
